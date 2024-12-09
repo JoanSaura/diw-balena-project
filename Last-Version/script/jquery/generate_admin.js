39b235094ace4594c71e611df1a2471e3d1138fe
Tommy <jsaura20489@iesjoanramis.org>
2024-12-09 21:45:37 +0100

    9.12

--- Last-Version/script/jquery/generate_admin.js
+++ Last-Version/script/jquery/generate_admin.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     const buttonsHtml = `
       <div class="button-container">
         <h1>Pàgina d'administrador</h1>
-        <a href="/html/admin/create_users.html">
+        <a href="../html/admin/create_users.html">
           <button id="create-user-btn" class="button-link">Creació d'Usuaris</button>
         </a>
-        <a href="/html/admin/edit_users.html">
+        <a href="../html/admin/edit_users.html">
           <button id="edit-user-btn" class="button-link">Edició d'Usuaris</button>
         </a>
       </div>
