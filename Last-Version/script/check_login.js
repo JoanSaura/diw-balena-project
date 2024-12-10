2985efada7b1868939bbaa375041a588c356dffc
TommyDrug <jsaura20489@iesjoanramis.org>
2024-12-10 19:03:52 +0100

    user name display

--- Last-Version/script/check_login.js
+++ Last-Version/script/check_login.js
@@ -3,9 +3,22 @@ $(document).ready(() => {
   const loginName = document.getElementById("username");
   const userLink = document.getElementById("user-link");
   
+  if (currentUser) {
     if (currentUser.is_first_login) {
       window.location.href = "../html/change_password.html";
     }
-    loginName.innerText = currentUser.name;  
-    userLink.style.display= 'none';
+
+    const userProfileLink = document.createElement('a');
+    userProfileLink.href = "./html/admin_page.html"; 
+    userProfileLink.innerText = currentUser.name;  
+    userProfileLink.classList.add("link_user");
+
+    loginName.innerHTML = '';  
+    loginName.appendChild(userProfileLink);  
+
+    userLink.style.display = 'none';
+  } else {
+    loginName.innerText = '';  
+    userLink.style.display = 'block'; 
+  }
 });
