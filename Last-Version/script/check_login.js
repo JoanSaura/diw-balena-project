c02d2143aea5848ae033ec9dd1db50653e0883eb
TommyDrug <jsaura20489@iesjoanramis.org>
2024-12-10 19:05:49 +0100

    changes

--- Last-Version/script/check_login.js
+++ Last-Version/script/check_login.js
@@ -1,24 +1,13 @@
 $(document).ready(() => {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
   const loginName = document.getElementById("username");
-  const userLink = document.getElementById("user-link");
   
-  if (currentUser) {
+  if (!currentUser) {
+    window.location.href = "../html/login.html";
+  } else {
     if (currentUser.is_first_login) {
       window.location.href = "../html/change_password.html";
     }
-
-    const userProfileLink = document.createElement('a');
-    userProfileLink.href = "./html/admin_page.html"; 
-    userProfileLink.innerText = currentUser.name;  
-    userProfileLink.classList.add("link_user");
-
-    loginName.innerHTML = '';  
-    loginName.appendChild(userProfileLink);  
-
-    userLink.style.display = 'none';
-  } else {
-    loginName.innerText = '';  
-    userLink.style.display = 'block'; 
+    loginName.innerText = currentUser.name;  
   }
 });
