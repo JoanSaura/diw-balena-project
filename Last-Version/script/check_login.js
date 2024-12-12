2592b90e88ad98151f932a08b593347defe82a73
TommyDrug <jsaura20489@iesjoanramis.org>
2024-12-12 17:18:25 +0100

    12/12

--- Last-Version/script/check_login.js
+++ Last-Version/script/check_login.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
   const loginName = document.getElementById("username");
-  
+  const UserCont = document.getElementById("user-cont");
 
 });
